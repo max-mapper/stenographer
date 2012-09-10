55db31e54e6ef460614c129c840ccb479e5fc202
Max Ogden <mogden@gmail.com>
2012-09-10 12:06:45 -0700

    handle EPIPE errors by upgrading concat-stream

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var path = require('path')
 var spawn = require('child_process').spawn;
 var concat = require('concat-stream')
 
-var BufferedStream = require('morestreams').BufferedStream;
-
 var args = process.argv.slice(2, process.argv.length)
 if (process.stdin.isTTY) {
   synthesizeAudio(args, function(err, flacStream) {
@@ -16,36 +14,40 @@ if (process.stdin.isTTY) {
     stenographify(flacStream, sampleRate, logResults)
   })
 } else {
-  process.stdin.pipe(concat(function(err, audio) {
-    var sampleRate
-    var duration
-    var trimStart = process.env['TRIMSTART'] || 0
-    var chunkSize = process.env['CHUNKSIZE'] || 10
-    chunkSize = +chunkSize
-    trimStart = +trimStart
-    var trimEnd = trimStart + chunkSize
-    getDuration(audio, function(err, _duration) {
-      if (err) console.error(err)
-      duration = _duration
-      if (sampleRate) convert()
+  var concatStream = concat(processAudio)
+  process.stdin.pipe(concatStream)
+}
+
+function processAudio(err, audio) {
+  var sampleRate
+  var duration
+  var trimStart = process.env['TRIMSTART'] || 0
+  var chunkSize = process.env['CHUNKSIZE'] || 5
+  chunkSize = +chunkSize
+  trimStart = +trimStart
+  var trimEnd = trimStart + chunkSize
+  getDuration(audio, function(err, _duration) {
+    if (err) console.error(err)
+    duration = _duration
+    if (sampleRate) convert()
+  })
+  getSampleRate(audio, function(err, _sampleRate) {
+    if (err) console.error(err)
+    sampleRate = _sampleRate
+    if (duration) convert()
+  })
+  function convert() {
+    var flacStream = flacConvertStream(args, trimStart, trimEnd)
+    stenographify(flacStream, audio, sampleRate, function(err, output) {
+      if (err) return console.log('stenographify error!', err)
+      process.stdout.write(output + " ")
+      if (trimEnd < duration) {
+        trimStart += chunkSize
+        trimEnd += chunkSize
+        convert()
+      }
     })
-    getSampleRate(audio, function(err, _sampleRate) {
-      if (err) console.error(err)
-      sampleRate = _sampleRate
-      if (duration) convert()
-    })
-    function convert() {
-      var flacStream = flacConvertStream(args, audio, trimStart, trimEnd)
-      stenographify(flacStream, sampleRate, function(err, output) {
-        if (err) return console.log(err)
-        if (trimEnd < duration) {
-          trimStart += chunkSize
-          trimEnd += chunkSize
-          convert()
-        }
-      })
-    }
-  }))
+  }
 }
 
 function logResults(err, output) {
@@ -82,18 +84,28 @@ function getSampleRate(audio, cb) {
   }))
 }
 
-function flacConvertStream(args, audio, start, end) {
-  var flacStream = spawn('sox', args.concat(['-', '-t', 'flac', '-', 'trim', start || '0', end || '10']))
-  flacStream.stdin.write(audio)
+function flacConvertStream(args, start, end) {
+  var flacStream = spawn('sox', args.concat(['-', '-t', 'flac', '-', 'trim', start || '0', end || '5']))
   return flacStream
 }
 
-function stenographify(flacStream, sampleRate, cb) {
+function stenographify(flacStream, audio, sampleRate, cb) {
   var totallyOpenVoiceEncodingAPI = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US"
-  var upload = request.post({'url': totallyOpenVoiceEncodingAPI, "headers": {"content-type": "audio/x-flac; rate=" + sampleRate.toString().split('\n')[0]}})
+  var opts = {'url': totallyOpenVoiceEncodingAPI, "headers": {"content-type": "audio/x-flac; rate=" + sampleRate.toString().split('\n')[0]}}
+  var upload = request.post(opts)
+
   flacStream.stdout.pipe(upload).pipe(concat(function(err, resp) {
-    var output = JSON.parse(resp)
-    if (output.hypotheses && output.hypotheses.length > 0 && output.hypotheses[0].utterance) output = output.hypotheses[0].utterance
+    if (err) return cb(err)
+    resp = resp.toString()
+    try {
+      var output = JSON.parse(resp)
+    } catch(e) {
+      var output = resp
+    }
+    if ( output.hypotheses && output.hypotheses.length > 0 && output.hypotheses[0].utterance ) {
+      output = output.hypotheses[0].utterance
+    }
     cb(false, output)
-  }))    
+  }))
+  flacStream.stdin.write(audio)
 }
