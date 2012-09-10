f8df76b8205f006df9f09197ba7faba35963e8d3
Max Ogden <mogden@gmail.com>
2012-09-09 17:59:26 -0700

    check in software to git repository

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var request = require('request')
+var os = require('os')
+var fs = require('fs')
+var path = require('path')
+var spawn = require('child_process').spawn;
+var words = process.argv.slice(2,process.argv.length)
+var tmpfile = path.join(os.tmpDir(), 'pizza.aiff')
+var aiffWriter = spawn('say', words.concat(['-o', tmpfile]))
+aiffWriter.on('close', function() {
+  var flacStream = spawn('sox', [tmpfile, '-t', 'flac', '-'])
+  var totallyOpenVoiceEncodingAPI = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US"
+  var upload = request.post({'url': totallyOpenVoiceEncodingAPI, "headers": {"content-type": "audio/x-flac; rate=16000"}})
+  flacStream.stdout.pipe(upload).pipe(process.stdout)
+})
---
+++ readme.md
@@ -0,0 +1,6 @@
+highly advanced voice recognition software written with Java scripts
+
+    npm install stenographer -g
+    stenographer hello yes this is dog
+
+MIT LICENSED
